4544f0de2ba6c97b9129e995273e11893ec579fa
noaha27 <noahddk@gmail.com>
2020-10-26 17:01:24 -0700

    video 28-33 submit

---
+++ NotelyRestApi/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotelyRestApi.Controllers.Models
+{
+    public class Note
+    {
+        public long Id { get; set; }
+        public string Subject { get; set; }
+        public String Detail { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
